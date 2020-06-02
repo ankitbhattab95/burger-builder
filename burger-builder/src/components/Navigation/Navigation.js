915a4b58165ec8f0359ca0eeb51dcb5699449a78
ankitbhattab95 <ankitbhatt.ab95@gmail.com>
2020-06-02 12:23:05 +0530

    test

--- burger-builder/src/components/Navigation/Navigation.js
+++ burger-builder/src/components/Navigation/Navigation.js
@@ -4,14 +4,15 @@ import classes from './Navigation.css'
 import logoSvg from '../../Assets/Images/logo.svg'
 const Navigation = (props) => {
     return (
-        <div className='anav'>
+        <div>
             
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                 <a className="navbar-brand" href="#">
                     <img src={logoSvg} alt="Buder builder"></img>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" onClick={props.showSidebar} data-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span><img src="https://img.icons8.com/fluent/48/000000/menu--v1.png"/></span>
+                    
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedConten" >
