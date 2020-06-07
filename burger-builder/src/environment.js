19f022c2896ce76555e3e155620c440e9302559e
ankitbhattab95 <ankitbhatt.ab95@gmail.com>
2020-06-07 13:48:41 +0530

    updated environment.js

--- burger-builder/src/environment.js
+++ burger-builder/src/environment.js
@@ -2,11 +2,10 @@ let baseURL = ''
 const readEnv = () => {
     if (process.env.NODE_ENV === 'production') {
         baseURL = 'https://buildtheburger.herokuapp.com/'
-        console.log(`${process.env.NODE_ENV} >>> baseurl =${baseURL}`)
     }
     else {
         baseURL = 'http://localhost:4000'
-        console.log(`${process.env.NODE_ENV} >>> baseurl =${baseURL}`)
     }
+    console.log(`${process.env.NODE_ENV} >>> baseurl =${baseURL}`)
 }
 export { readEnv, baseURL };
