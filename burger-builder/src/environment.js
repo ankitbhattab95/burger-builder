b8a877e75ca72965637b80ab9cde9b489e39e63c
ankitbhattab95 <ankitbhatt.ab95@gmail.com>
2020-06-07 13:28:06 +0530

    test

--- burger-builder/src/environment.js
+++ burger-builder/src/environment.js
@@ -1,14 +1,15 @@
 let baseURL = ''
 const readEnv = () => {
     // process.env.SRV_URL = baseURL
-    if (process.env.SRV_URL !== undefined) {
-        baseURL = process.env.SRV_URL
+    // if (process.env.SRV_URL !== undefined) {
+    if (process.env.NODE_ENV === 'production') {
+        baseURL = 'https://buildtheburger.herokuapp.com/'
         console.log('if >>> setting baseURL to ', process.env.SRV_URL)
         console.log('if >>> baseurl = ', baseURL)
     }
     else {
         baseURL = 'http://localhost:4000'
-        console.log('else >>> setting baseURL to ', process.env.SRV_URL)
+        console.log('else >>> setting baseURL to ', process.env.REACT_APP_SRV_URL, process.env.NODE_ENV)
         console.log('else >>> baseurl = ', baseURL)
     }
 }
