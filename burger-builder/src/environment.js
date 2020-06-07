97298dcca03e0826da1d003fb039852fd868d33f
ankitbhattab95 <ankitbhatt.ab95@gmail.com>
2020-06-07 12:02:41 +0530

    test

--- burger-builder/src/environment.js
+++ burger-builder/src/environment.js
@@ -2,9 +2,9 @@ let baseURL = 'http://localhost:4000'
 const readEnv = () => {
     process.env.SRV_URL = baseURL
     // console.log('process.env.SRV_URL = ', process.env.SRV_URL)
-    if (process.env.SRV_URL) {
+    if (process.env.SRV_URL != undefined) {
         baseURL = process.env.SRV_URL
     }
-    // console.log('baseurl = ', baseURL)
+    console.log('baseurl = ', baseURL)
 }
 export { readEnv, baseURL };
